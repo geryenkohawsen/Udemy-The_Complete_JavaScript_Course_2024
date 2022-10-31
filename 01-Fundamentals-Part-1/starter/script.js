13fd0efc0f070ffeb01d19175d8f0d89cf64b10e
Geryenko Hawsen <gery.zarons@gmail.com>
2022-10-31 22:34:07 +0900

    10. Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,23 @@
 let js = 'amazing';
-if (js === 'amazing') {
-	alert('JavaScript is FUN!');
-}
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Gery';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+
+let _function = 'todo';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let mySecondJob = 'Teacher';
+
+let jon1 = 'Programmer';
+let job2 = 'Teacher';
+
+console.log(myFirstJob);
