98a6cb707104fe074d75c5c2f8a3b4513dcd8cb9
Geryenko Hawsen <gery.zarons@gmail.com>
2022-11-02 21:56:37 +0900

    15. Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -62,7 +62,7 @@ lastName = 'Hawsen';
 console.log(lastName);
 console.log(typeof lastName);
 */
-
+/*
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -90,3 +90,17 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
