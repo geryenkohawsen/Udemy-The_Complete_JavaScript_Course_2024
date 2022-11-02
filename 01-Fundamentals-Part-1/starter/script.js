00cbc7001816f712e19acc2840695ba099e0a816
Geryenko Hawsen <gery.zarons@gmail.com>
2022-11-02 21:32:04 +0900

    14. Basic Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -48,7 +48,7 @@ let testNull = null;
 console.log(testNull);
 console.log('test', typeof testNull);
 */
-
+/*
 let age = 30;
 age = 31;
 
@@ -61,3 +61,32 @@ job = 'teacher';
 lastName = 'Hawsen';
 console.log(lastName);
 console.log(typeof lastName);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Geryenko';
+const lastName = 'Hawsen';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators 代入演算子
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparison operators 比較演算子
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
