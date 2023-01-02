c0d1f34487a48393fc8ec6452488f37034bb8831
Geryenko Hawsen <gery.zarons@gmail.com>
2023-01-02 20:46:42 +0900

    32. Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive!!');
+
+// const interface = 'Audio';
+// const private = 123;
+*/
