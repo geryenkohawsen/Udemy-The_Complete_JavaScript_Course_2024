6c5f18d194453df792bb0a1249336de88bd8fe14
Geryenko Hawsen <gery.zarons@gmail.com>
2023-01-02 21:13:55 +0900

    33. Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,3 +10,26 @@ if (hasDriversLicense) console.log('I can drive!!');
 // const interface = 'Audio';
 // const private = 123;
 */
+
+function logger() {
+	console.log('My name is Gery');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+	console.log(apples, oranges);
+	const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+	return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
