ae095a757f59d1f97b606f2b877f0034dcc614fc
Geryenko Hawsen <gery.zarons@gmail.com>
2023-01-03 15:25:04 +0900

    40. Basic Array Operations (Methods)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -151,6 +151,7 @@ console.log(checkWinner(avgDolphins, avgKoalas));
 console.log(checkWinner(123, 11)); // test
 */
 
+/*
 const friend1 = 'Michael';
 const friend2 = 'Steven';
 const friend3 = 'Peter';
@@ -186,3 +187,34 @@ console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years.length - 1)];
 console.log(ages);
+*/
+
+// Add elements
+const friends = ['Michael', 'Steven', 'Peter']; // add element to the back
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John'); // add element to the front
+console.log(friends);
+
+// Remove elements
+friends.pop(); // remove last element
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // remove first element
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+	console.log('You have a friend called Steven');
+}
