31a2c8257f5614c01fa8a0bfa32053a9868afda0
Geryenko Hawsen <gery.zarons@gmail.com>
2023-01-03 15:41:42 +0900

    42. Introduction to Objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -231,6 +231,7 @@ if (friends.includes('Steven')) {
 // Test data: 125, 555 and 44
 // Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
 
+/*
 const calcTip = function (bill) {
 	return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
@@ -240,3 +241,20 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 console.log(bills, tips);
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(totals);
+*/
+
+const jonasArray = [
+	'Jonas',
+	'Schmedtmann',
+	2037 - 1991,
+	'teacher',
+	['Michael', 'Steven', 'Peter'],
+];
+
+const jonas = {
+	firstName: 'Jonas',
+	lastName: 'Schmedtmann',
+	age: 2037 - 1991,
+	job: 'teacher',
+	friends: ['Michael', 'Steven', 'Peter'],
+};
