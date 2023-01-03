981e2c76c13c97d6629befd807a30eb709ea820d
Geryenko Hawsen <gery.zarons@gmail.com>
2023-01-03 11:13:57 +0900

    34. Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,6 +11,7 @@ if (hasDriversLicense) console.log('I can drive!!');
 // const private = 123;
 */
 
+/*
 function logger() {
 	console.log('My name is Gery');
 }
@@ -33,3 +34,18 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+*/
+
+// Function declaration ❗❕
+function calcAge1(birthYear) {
+	return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// Function expression ❗❕
+const calcAge2 = function (birthYear) {
+	return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age2);
