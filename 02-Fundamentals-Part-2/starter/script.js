e1932932160a26c14118c4acdc5e8217eb8f6059
Geryenko Hawsen <gery.zarons@gmail.com>
2023-01-03 16:32:54 +0900

    44. Object Methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -253,6 +253,7 @@ const jonasArray = [
 ];
 */
 
+/*
 const jonas = {
 	firstName: 'Jonas',
 	lastName: 'Schmedtmann',
@@ -288,3 +289,40 @@ console.log(jonas);
 console.log(
 	`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
 );
+*/
+
+const jonas = {
+	firstName: 'Jonas',
+	lastName: 'Schmedtmann',
+	birthYear: 1991,
+	job: 'teacher',
+	friends: ['Michael', 'Steven', 'Peter'],
+	hasDriversLicense: true,
+
+	// calcAge: function (birthYear) {
+	// 	return 2037 - birthYear;
+	// },
+
+	// calcAge: function () {
+	// 	// console.log(this);
+	// 	return 2037 - this.birthYear;
+	// },
+
+	calcAge: function () {
+		this.age = 2027 - this.birthYear;
+		return this.age;
+	},
+
+	getSummary: function () {
+		return `${jonas.firstName} is a ${this.calcAge()}-year old ${
+			this.job
+		}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`;
+	},
+};
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+console.log(jonas.getSummary());
