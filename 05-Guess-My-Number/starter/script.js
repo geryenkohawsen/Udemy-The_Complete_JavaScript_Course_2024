39077015c21f0bb606345f8e5dd8b196bef11324
Geryenko Hawsen <gery.zarons@gmail.com>
2023-01-11 00:19:13 +0900

    77. Implementing Highscores

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,6 +14,7 @@ console.log((document.querySelector('.guess').value = 23));
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -30,6 +31,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
   // When guess is too high
   else if (guess > secretNumber) {
