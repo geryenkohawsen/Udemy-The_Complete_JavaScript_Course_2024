28d9f64aed2de71de95f16be6f701d291399de3d
Geryenko Hawsen <gery.zarons@gmail.com>
2023-01-11 23:59:10 +0900

    80. Working With Classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,4 +6,21 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) console.log(btnsOpenModal[i]);
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
