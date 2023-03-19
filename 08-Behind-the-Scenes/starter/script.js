6947768aafd39ac03167f342c0e59109ab07f645
Geryenko Hawsen <gery.zarons@gmail.com>
2023-03-19 21:01:44 +0900

    97. The this Keyword in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -37,6 +37,7 @@
 // // console.log(age);
 // // printAge();
 
+/*
 // Variables
 console.log(me);
 // console.log(job);
@@ -83,3 +84,39 @@ const z = 3;
 console.log(x === window.x);
 console.log(x === window.y);
 console.log(x === window.z);
+*/
+
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+    return 'return result of calcAge';
+  },
+  thisIs: this,
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
