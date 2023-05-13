4245123caf48cc8edb92e42cb4995cd48aced84e
Geryenko Hawsen <gery.zarons@gmail.com>
2023-05-13 23:37:38 +0900

    124. [TRY] Coding Challenge #4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,6 +4,65 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below),
+and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+const textareaEl = document.querySelector('textarea');
+const btnEl = document.querySelector('button');
+btnEl.addEventListener('click', () => {
+  const textareaVal = textareaEl.value;
+  const textareaValArr = textareaVal.split('\n');
+  for (const [index, val] of Object.entries(textareaValArr)) {
+    const cleanVal = val.trim().toLowerCase();
+    const underscoreIdx = cleanVal.indexOf('_');
+    const combinedVal = cleanVal.split('_').join('');
+    const namesUpper = [];
+    for (const [index, n] of Object.entries(combinedVal)) {
+      if (Number(index) == underscoreIdx) namesUpper.push(n.toUpperCase());
+      else namesUpper.push(n);
+    }
+    console.log(
+      namesUpper
+        .join('')
+        .padEnd(20, ' ')
+        .padEnd(20 + (Number(index) + 1), '✅')
+    );
+  }
+});
+/*
 // SPLIT and JOIN
 console.log('a+very+nice+string'.split('+'));
 console.log('Geryenko Hawsen'.split(' '));
@@ -52,6 +111,7 @@ const planeInLine = function (n) {
 planeInLine(5);
 planeInLine(3);
 planeInLine(12);
+*/
 
 /*
 const airline = 'TAP Air Portugal';
