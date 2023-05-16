38ac0f901a82c0f98b1b5e4f13267a62c8c7b09f
Geryenko Hawsen <gery.zarons@gmail.com>
2023-05-16 23:15:47 +0900

    128. Default Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const bookings = [];
+const basePrice = 199;
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = basePrice * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH345', 2, 800);
+createBooking('LH345', undefined, 300); // use undefined to skip parameter that we want to use the default
+createBooking('LH345', 5);
