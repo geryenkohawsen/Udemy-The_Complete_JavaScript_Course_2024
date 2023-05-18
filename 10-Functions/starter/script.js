dc01bb8b872d16aac4f4845abcc520979f5b7901
Geryenko Hawsen <gery.zarons@gmail.com>
2023-05-18 23:52:21 +0900

    129. How Passing Arguments Works: Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/////////////////////////
+/*
 const bookings = [];
 const basePrice = 199;
 
@@ -21,3 +23,38 @@ createBooking('LH123');
 createBooking('LH345', 2, 800);
 createBooking('LH345', undefined, 300); // use undefined to skip parameter that we want to use the default
 createBooking('LH345', 5);
+*/
+
+const flight = 'LH234';
+const gery = {
+  name: 'Geryenko Hawsen',
+  passport: 123123123,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH399';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 123123123) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, gery);
+console.log(flight);
+console.log(gery);
+
+const flightNum = flight;
+const passenger = gery;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000);
+};
+
+newPassport(gery);
+checkIn(flight, gery);
+
+console.log(flight);
+console.log(gery);
