14455be51343e1a37a3c911a9bccec9d7e42799f
Geryenko Hawsen <gery.zarons@gmail.com>
2023-07-09 17:15:55 +0900

    153. The reduce Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -82,6 +82,12 @@ const displayMovements = movements => {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+calcDisplayBalance(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach(acc => {
     acc.username = acc.owner
@@ -310,6 +316,7 @@ GOOD LUCK 😀
 // );
 // console.log(movementDescriptions);
 
+/*
 const deposits = movements.filter(function (mov, i, arr) {
   return mov > 0;
 });
@@ -324,3 +331,25 @@ console.log(depositsFor);
 
 const withdrawal = movements.filter(mov => mov < 0);
 console.log(withdrawal);
+*/
+
+console.log(movements);
+
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc} + ${cur }`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log('balance → ', balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log('balance2 → ', balance2);
+
+///// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov; // return current movement as the accumulator
+}, movements[0]);
+console.log('max → ', max);
