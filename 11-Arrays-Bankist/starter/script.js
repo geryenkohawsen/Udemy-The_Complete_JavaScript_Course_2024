fed7f82b6b65a060884ac1e921dfe99c006957e9
Geryenko Hawsen <gery.zarons@gmail.com>
2023-07-09 22:49:09 +0900

    156. [ANSWER] Coding Challenge #3

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -504,3 +504,19 @@ console.log(
   'test data 2 → ',
   calcAverageHumanAgeChaining([16, 6, 10, 5, 6, 1, 4])
 );
+
+///// Lecture answer
+const calcAverageHumanAgeChainingAns = ages =>
+  ages
+    .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+console.log(
+  'test data 1 → ',
+  calcAverageHumanAgeChainingAns([5, 2, 4, 1, 15, 8, 3])
+);
+console.log(
+  'test data 2 → ',
+  calcAverageHumanAgeChainingAns([16, 6, 10, 5, 6, 1, 4])
+);
