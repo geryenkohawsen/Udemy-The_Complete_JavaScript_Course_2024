f84bc2788492be91be1d8c2ff09026d5f24a6cca
Geryenko Hawsen <gery.zarons@gmail.com>
2023-10-04 00:19:31 +0900

    171. Math and Rounding

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -252,6 +252,7 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+/*
 console.log(23 === 23.0);
 
 // 3/10 === 3.33333333
@@ -283,3 +284,46 @@ console.log(Number.isFinite(20)); // true
 console.log(Number.isFinite('20')); // false
 console.log(Number.isFinite(+'xx20')); // false
 console.log(Number.isFinite(23 / 0)); // false // infinity is not finite
+*/
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(1, 2, 13, 4, 5));
+console.log(Math.max(1, 2, '13', 4, 5)); // will do conversion for us
+console.log(Math.max(1, 2, '13px', 4, 5)); // will NOT do parsing
+
+console.log(Math.min(1, 2, 13, 4, 5));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+function randomInt(min, max) {
+  return Math.trunc(Math.random() * (max - min) + 1) + min;
+}
+console.log(randomInt(7, 15));
+
+// Rounding integers (auto conversion for string)
+console.log('Rounding integers');
+console.log(Math.round(-10.3)); // -10
+console.log(Math.round(10.3)); // 10
+console.log(Math.round(10.5)); // 11
+console.log(Math.round('10.9')); // 11
+
+console.log(Math.ceil(-10.3)); // -10
+console.log(Math.ceil(10.3));
+console.log(Math.ceil(10.5));
+console.log(Math.ceil('10.9'));
+
+console.log(Math.floor(-10.3)); // -11
+console.log(Math.floor(10.3));
+console.log(Math.floor(10.5));
+console.log(Math.floor(10.9));
+
+// Rounding decimals (return value will be a string)
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.7315).toFixed(2));
+console.log(+(2.7315).toFixed(2)); // change return value back into a number
