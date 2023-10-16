63a5210b8ad1b9026a8132b9c30f7af9351c316e
Geryenko Hawsen <gery.zarons@gmail.com>
2023-10-17 01:15:44 +0900

    177. Operations With Dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -23,7 +23,7 @@ const account1 = {
     '2020-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
     '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-10-14T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -81,6 +81,23 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+function formatMovementDate(date, noText) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const dayPassed = calcDaysPassed(new Date(), date);
+
+  if (noText || dayPassed > 7) {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+  if (dayPassed === 0) return 'Today';
+  if (dayPassed === 1) return 'Yesterday';
+  if (dayPassed <= 7) return `${dayPassed} days ago`;
+}
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -91,17 +108,13 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-    const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-
+    const date = formatMovementDate(new Date(acc.movementsDates[i]));
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__date">${day}/${month}/${year}</div>
+        <div class="movements__date">${date}</div>
         <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
@@ -170,12 +183,7 @@ containerApp.style.opacity = 100;
 
 // Create current date and time
 const now = new Date();
-const day = `${now.getDate()}`.padStart(2, 0);
-const month = `${now.getMonth() + 1}`.padStart(2, 0);
-const year = now.getFullYear();
-const hour = `${now.getHours()}`.padStart(2, 0);
-const minute = `${now.getMinutes()}`.padStart(2, 0);
-labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minute}`;
+labelDate.textContent = formatMovementDate(now, true);
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -465,3 +473,10 @@ console.log('Date.now() --> ', Date.now());
 console.log('setFullYear --> ', future.setFullYear(2040));
 console.log('new future --> ', future);
 */
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log('future --> ', future);
+console.log('+future --> ', +future);
+
+const days1 = calcDaysPassed(new Date(2037, 10, 19), new Date(2037, 10, 9));
+console.log('days1 --> ', days1);
