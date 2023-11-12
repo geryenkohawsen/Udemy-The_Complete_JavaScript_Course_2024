f61ba520c8c9aa1cf533c0ce094e1fdaaae4deae
Geryenko Hawsen <gery.zarons@gmail.com>
2023-11-12 13:06:54 +0900

    208. Constructor Functions and the new Operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const gery = new Person('Gery', 1999);
+console.log(gery);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1997);
+console.log(matilda, jack);
+
+console.log('instanceof --> ', gery instanceof Person);
