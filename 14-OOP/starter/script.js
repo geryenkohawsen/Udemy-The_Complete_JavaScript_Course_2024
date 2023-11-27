302b5a6c6bdbe96fd23f6d3c1fe10e51c8c21e2a
Geryenko Hawsen <gery.zarons@gmail.com>
2023-11-27 23:48:46 +0900

    223. Encapsulation: Protected Properties and Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -477,19 +477,26 @@ jay.calcAge();
 */
 
 class Account {
+  #movements = [];
+  #pin; // legacy code will write this._pin = pin inside the constructor
+  #locale;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    this.#locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${this.owner}`);
   }
 
   // Public Interface AKA an API
+  getMovements() {
+    return this.#movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
@@ -497,12 +504,12 @@ class Account {
   }
 
   // This is an internal method that should not be exposed
-  approveLoan(val) {
+  #approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this.#approveLoan(val)) {
       this.deposit(val);
       console.log('Loan approved');
     }
@@ -511,12 +518,12 @@ class Account {
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1.movements.push(250); // not good because it is not encapsulated
+// acc1.movements.push(-140); // not good because it is not encapsulated
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+// acc1.#approveLoan(1000); // will cause error because it is a private method
 
 console.log(acc1);
 console.log(acc1.pin); // Should not be accessible from outside the class
