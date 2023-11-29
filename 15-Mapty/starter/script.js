9164df2e772e315bc43654efbf53a4f12f072ae9
Geryenko Hawsen <gery.zarons@gmail.com>
2023-11-29 23:42:37 +0900

    232. Using the Geolocation API

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,15 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    console.log(
+      `https://www.google.co.jp/maps/@${latitude},${longitude},15z?hl=en&entry=ttu`
+    );
+  },
+  function () {
+    alert('Could not get your position');
+  }
+);
