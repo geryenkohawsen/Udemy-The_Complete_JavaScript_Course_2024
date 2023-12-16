ab5c49c3464d4db2f0664bc18f3c228b467070cd
Geryenko Hawsen <gery.zarons@gmail.com>
2023-12-17 03:05:20 +0900

    250. Welcome to Callback Hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -6,17 +6,9 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 // https://restcountries.com/v2
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest(); // Old way of doing HTTP requests
-  request.open('GET', `https://restcountries.com/v2/name/${country}`); // Open the request (next step is to send it)
-  request.send(); // Send the request
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `
-  <article class="country">
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
@@ -29,11 +21,42 @@ const getCountryData = function (country) {
     </div>
   </article>
   `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbor = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest(); // Old way of doing HTTP requests
+  request.open('GET', `https://restcountries.com/v2/name/${country}`); // Open the request (next step is to send it)
+  request.send(); // Send the request
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country (1st request)
+    renderCountry(data);
+
+    // Get neighbor country (2nd request)
+    const neighbor = data.borders?.[0];
+    if (!neighbor) return;
+
+    // AJAX call country 2
+    console.log('neighbor → ', neighbor);
+    const request2 = new XMLHttpRequest(); // Old way of doing HTTP requests
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbor}`); // Open the request (next step is to send it)
+    request2.send(); // Send the request
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // Render country (2st request)
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('germany');
+// getCountryAndNeighbor('portugal');
+getCountryAndNeighbor('usa');
