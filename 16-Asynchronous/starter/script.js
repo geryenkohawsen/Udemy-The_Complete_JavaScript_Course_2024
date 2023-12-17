824dfd8491da5939ed9babd1f9bae0f5588cc167
Geryenko Hawsen <gery.zarons@gmail.com>
2023-12-18 00:00:02 +0900

    258. The Event Loop in Practice

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -177,6 +177,7 @@ TEST COORDINATES 2: -33.933, 18.474
 
 GOOD LUCK 😀
 */
+/*
 const freeAPI = '640766658316374271839x31894';
 
 const whereAmI = function (lat, lng) {
@@ -206,3 +207,15 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+console.log('Test start'); // 1st
+setTimeout(() => console.log('0 sec timer'), 0); // 5th (callback queue)
+Promise.resolve('Resolved promise 1').then(res => console.log(res)); // 3rd (microtask queue)
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res); // 4rd (microtask queue)
+});
+
+console.log('Test end'); // 2nd
