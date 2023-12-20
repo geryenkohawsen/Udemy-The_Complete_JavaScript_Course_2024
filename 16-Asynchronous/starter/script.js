17186f78a2ad433cb5d4124fd73f93085c02b3ff
Geryenko Hawsen <gery.zarons@gmail.com>
2023-12-21 01:50:27 +0900

    265. Running Promises in Parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -396,6 +396,7 @@ createImage('img/img-1.jpg')
 //   .then(res => res.json())
 //   .then(data => console.log(data));
 
+/*
 const freeAPI = '640766658316374271839x31894';
 
 const getPosition = function () {
@@ -458,3 +459,24 @@ console.log('3.2: Finished getting location');
   console.log(`3.1: Finished getting location`);
 })();
 console.log('3.2: Finished getting location');
+*/
+
+const get3countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    renderError(`Something went wrong 💥💥💥 ${err.message}`);
+  }
+};
+
+get3countries('portugal', 'brazil', 'argentina');
