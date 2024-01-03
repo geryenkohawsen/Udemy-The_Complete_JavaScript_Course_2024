59be440e4463920820f696370e92f8aa769961ba
Geryenko Hawsen <gery.zarons@gmail.com>
2024-01-04 01:08:27 +0900

    283. Let's Fix Some Bad Code: Part 2

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,6 @@
-const budget = [
+'strict mode';
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,44 +9,86 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+
+spendingLimits.jay = 300;
+console.log('spendingLimits --> ', spendingLimits);
+
+const getLimit = (limits, user) => spendingLimits?.[user] ?? 0;
+
+// PURE FUNCTION
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value, description, user: cleanUser }]
+    : state;
+
+  // with object literal syntax, if prop name is same as variable name, we can just put it as it is
+  // budget.push({ value: -value, description, user: cleanUser });
 };
 
-const getLimit = user => spendingLimits?.[user] ?? 0;
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+console.log('newBudget1 --> ', newBudget1);
 
-const addExpense = function (value, description, user = 'jonas') {
-  user = user.toLowerCase();
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+console.log('newBudget2 --> ', newBudget2);
 
-  const limit = getLimit(user);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+console.log('newBudget3 --> ', newBudget3);
 
-  if (value <= limit) {
-    //* with object literal syntax, if prop name is same as variable name, we can just put it as it is
-    // budget.push({ value: -value, description: description, user: user });
-    budget.push({ value: -value, description, user });
-  }
-};
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
+// const checkExpenses2 = function (state, limits) {
+//   return state.map(entry => {
+//     return entry.value < -getLimit(entry.user)
+//       ? { ...entry, flag: 'limit' }
+//       : entry;
+//   });
+//   // for (const entry of budget)
+//   //   if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+// };
 
-const checkExpenses = function () {
-  for (const entry of budget)
-    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
-};
-checkExpenses();
+// PURE FUNCTION (with arrow function) that does not mutate anything
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(entry.user) ? { ...entry, flag: 'limit' } : entry
+  );
 
-const logBigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget)
-    output +=
-      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log('finalBudget --> ', finalBudget);
+
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+  // .reduce((acc, cur) => `${acc} / ${cur.description.slice(-2)}`, ''); //* when using reduce
+
+  console.log('bigExpenses --> ', bigExpenses);
+
+  // let output = '';
+  // for (const entry of budget)
+  //   output +=
+  //     entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
 };
 
 console.log('after adding expenses', budget);
-logBigExpenses(1000);
+logBigExpenses(finalBudget, 500);
