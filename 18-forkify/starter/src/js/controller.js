bd8e2001683f2d6a80e45c529498d24263815a76
Geryenko Hawsen <gery.zarons@gmail.com>
2024-01-04 23:23:48 +0900

    288. Loading a Recipe from API

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,34 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await response.json();
+
+    console.log('response --> ', response);
+    console.log('data --> ', data);
+
+    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log('recipe --> ', recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+showRecipe();
