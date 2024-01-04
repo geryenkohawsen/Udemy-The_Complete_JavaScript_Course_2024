d7ee824499f21da70fd568caab4385d47e0da498
Geryenko Hawsen <gery.zarons@gmail.com>
2024-01-05 00:28:55 +0900

    290. Listening For load and hashchange Events

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -31,12 +31,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     //* 1. Loading recipe
     renderSpinner(recipeContainer);
 
     const response = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await response.json();
 
@@ -183,4 +186,6 @@ const showRecipe = async function () {
     alert(error);
   }
 };
-showRecipe();
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, showRecipe)
+);
